e77b8886c00b56dafa8db6e77337aee6c9e8d0b8
Phuc Dep Trai <nhatphucpham@hotmail.com>
2023-06-26 19:21:51 +0700

    fix app

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,8 +1,8 @@
-const apiId = 's54cp5j0o3'
+const apiId = '5murqtth1i'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
   domain: 'dev-sif3oiav3j1s6ypx.us.auth0.com',            // Auth0 domain
-  clientId: '3kPhTiYEbXuMMXZWNveEnEnxHrfuYdBi',          // Auth0 client id
+  clientId: '5784Pc5nqULukdVpqUe6rzuC5wypxOBU',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
